43654b48f8ddf58b294f03be60a0cff7d42d06b7
vaijulbalar <84895290+vaijulbalar@users.noreply.github.com>
2021-05-27 17:09:46 +0530

    day2

---
+++ day2/demo.php
@@ -0,0 +1,15 @@
+<?php
+    echo "hello";
+    echo "<h1>h1 tag inside html</h1>";
+?>
+
+<html>
+    <head>
+        <body>
+            <h1>this is html</h1>
+            <?php
+             echo "php in html";
+             ?>
+        </body>
+    </head>
+</html>
