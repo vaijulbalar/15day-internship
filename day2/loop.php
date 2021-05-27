ef16b9d9b23485bef4beacf285f08015d320196c
vaijulbalar <84895290+vaijulbalar@users.noreply.github.com>
2021-05-27 17:10:59 +0530

    day2

---
+++ day2/loop.php
@@ -0,0 +1,40 @@
+<?php
+
+ 
+//if loop
+ $a=1;
+ if($a>10){
+       echo "a > 10";}
+ else{
+  echo  "a<10";}
+
+  // while
+$a=5;
+while($a<=10){
+    $a++;
+    echo "<br>".$a;
+}
+ // for
+ for($i=1;$i<11;$i++){
+     echo "<br>".$i;
+ }
+ // switch
+  $b=4;
+  switch($b){
+      case 1:
+        echo "one";
+        break;
+      case 2:
+        echo "two";
+        break;
+      case 3:
+        echo "three";
+        break;
+      default :
+      echo "pls enter 1-3";
+
+    
+  }
+
+       
+ 
---
+++ day2/var.php
@@ -0,0 +1,14 @@
+<?php
+   
+   $a = 10;
+   $b = 20;
+    
+   //method 1
+   echo "the value of A is $a <br> the value of B is $b";
+   //method-2
+   echo "<br>the value of A is" .$a;
+   echo "<br>the value of B is" .$b;
+   //method-3
+   echo "<br>the value of A is ".$a. "<br> the value of B is" .$b;
+   //method-4
+   echo "<br>the sum is ".($a+$b);
